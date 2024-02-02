9e01d06985e1b1d49a599e9217d1f8884957c216
Nasra Omar <nasra.s.omar@hotmail.com>
2024-02-01 21:29:52 -0600

    completed group assignment

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -3,94 +3,116 @@ console.log('****** Part Supply *******');
 // For each question, be sure to use console.log statements to ensure that
 // your code does what you expect!
 
-// 1. Create a variable called 'partsNeeded' with a value of the number 40.
-let partsNeeded = 40;
+
+// 1. Create a variable called 'partsNeeded’ with a value of the number 40.
 console.log('1. Create partsNeeded:');
+let partsNeeded = 40;
+  console.log (partsNeeded);
 
-
-// 2. Create a variable called 'supplyChanges' whose value is an array containing
+// 2. Create a variable called 'supplyChanges’ whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
-let supplyChanges = (3, 5, -6, 0, 7, 11);
 console.log('2. Create supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log(supplyChanges);
 
-
-// 3. Create a variable called 'secondItem' and assign it the value of the second
-//    item in the 'supplyChanges' array.
-let secondItem = '';
-let newItem= 5;
-File.push(newItem);
-console.log('secondItemn: ', secondItem);
-
+// 3. Create a variable called 'secondItem’ and assign it the value of the second
+//    item in the 'supplyChanges’ array.
 console.log('3. Access the second value of supplyChanges:');
+let index = 1;
+let secondItem = supplyChanges[index];
+  console.log(secondItem);
 
-
-// 4. The last value in the 'supplyChanges' array was added by mistake.
-//    Remove it from the array and store it inside a new variable called 'removedItem'.
+// 4. The last value in the 'supplyChanges’ array was added by mistake.
+//    Remove it from the array and store it inside a new variable called 'removedItem’.
 console.log('4. Remove the last value from supplyChanges:');
-
+let removedItem = supplyChanges.pop();
+console.log(removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
+supplyChanges.push(25);
+console.log(supplyChanges);
 
-
-// 6. Create three new variables named 'positives', 'negatives', and
-//    'zeroes' whose values are empty arrays. Then, write a for loop
-//    that loops through the 'supplyChanges' array. For each iteration of
+// 6. Create three new variables named 'positives’, 'negatives’, and
+//    'zeroes’ whose values are empty arrays. Then, write a for loop
+//    that loops through the 'supplyChanges’ array. For each iteration of
 //    the loop:
-//      - If the value is a positive number, push it into the 'positives' array.
-//      - If the value is a negative number, push it into the 'negatives' array.
-//      - If the value is a zero, push it into the 'zeroes' array.
-let positive = [];
-let negatives = [] 
-let zeroes = [];
-for (let i = 0; i < supplyChanges.length; i++); {
-
-}
+//      - If the value is a positive number, push it into the 'positives’ array.
+//      - If the value is a negative number, push it into the 'negatives’ array.
+//      - If the value is a zero, push it into the 'zeroes’ array.
 console.log('6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:');
-
-
-
-
+let positives = [];
+let negatives = [];
+let zeroes = [];
+for (let index = 0; index < supplyChanges.length; index ++) {
+    if (supplyChanges[index] > 0) {
+          positives.push(supplyChanges[index])
+    }
+    if (supplyChanges[index] < 0) {
+          negatives.push(supplyChanges[index])
+    }
+    if (supplyChanges[index] === 0) {
+      zeroes.push(supplyChanges[index])
+    }
+}
+console.log(supplyChanges);
+console.log(positives);
+console.log(negatives);
+console.log(zeroes);
 // ***** STRETCH GOALS *********************************************
-// 7. Rewrite the 'for' loop from #6 as a 'for...of' loop. Instead of 'positives',
-//    'negatives', and 'zeroes', create three new arrays named 'stretchPositives',
-//    'stretchNegatives', and 'stretchZeroes'. 
-console.log('7. Looping through supplyChanges to populate more arrays with positive, negative, and zero values:');
-
-
-// 8. Create a variable called 'totalParts' and assign it a value of 0.
-//    Then, write a loop that adds each value of the 'supplyChanges'
-//    array to the 'totalParts' variable.
+// 7. Rewrite the 'for’ loop from #6 as a 'for...of’ loop. Instead of 'positives’,
+//    'negatives’, and 'zeroes’, create three new arrays named 'stretchPositives’,
+//    'stretchNegatives’, and 'stretchZeroes’.
+console.log('7. Looping through supplyChanges to populate more arrays with positive, negative, and zero values:'),
+    // let stretchPositives = [];
+    // let stretchNegatives = [];
+    // let stretchZeroes = [];
+    // for (let stretchPositives of positives);
+    // for (let stretchNegatives of negatives);
+    // for (let stretchZeroes of zeroes);
+    // console.log(stretchPositives);
+    // console.log(stretchNegatives);
+    // console.log(stretchZeroes);
+// I COULDN’T FIGURE OUT THIS STRETCH GOALS.
+// 8. Create a variable called 'totalParts’ and assign it a value of 0.
+//    Then, write a loop that adds each value of the 'supplyChanges’
+//    array to the 'totalParts’ variable.
 console.log('8. Looping through supplyChanges to calculate the sum:');
-
-
-// 9. We have a large stash of parts in our warehouse that we 
+let totalParts = 0;
+for (let index =0; index < supplyChanges.length; index ++) {
+  totalParts += supplyChanges[index];
+}
+console.log(totalParts);
+// 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment. There are 572
 //    parts in total, and each box holds 7 parts.
 //    Create two variables:
-//      1. 'parts': Set its value to 572
-//      2. 'boxesFilled': Set its value to 0
-//    Use a 'while' loop to keep adding parts to boxes UNTIL
+//      1. 'parts’: Set its value to 572
+//      2. 'boxesFilled’: Set its value to 0
+//    Use a 'while’ loop to keep adding parts to boxes UNTIL
 //    no more boxes can be filled. When the loop is finished,
-//    the value of 'parts' should reflect how many parts are
-//    "left over," and the value of 'boxesFilled' should reflect
+//    the value of 'parts’ should reflect how many parts are
+//    “left over,” and the value of 'boxesFilled’ should reflect
 //    how many boxes were filled.
-console.log('9. Filling boxes with a "while" loop');
-
-
-
-
+    // console.log('9. Filling boxes with a “while” loop’);
+    // let parts = 572;
+    // let boxesFilled = 0;
+    // while (boxesFilled <= parts) {
+    //   boxesFilled += 7;
+    // }
+    // console.log(boxesFilled);
+// I COULDN’T FIGURE OUT THIS STRETCH GOALS.
 // DO NOT MODIFY
 // Used for automated testing
 try {
   module.exports = {
-    partsNeeded: typeof partsNeeded !== 'undefined' ? partsNeeded : undefined,
+    partsNeeded: typeof partsNeeded !== 'undefined'? partsNeeded : undefined,
     supplyChanges: typeof supplyChanges !== 'undefined' ? supplyChanges : undefined,
     secondItem: typeof secondItem !== 'undefined' ? secondItem : undefined,
     removedItem: typeof removedItem !== 'undefined' ? removedItem : undefined,
     positives: typeof positives !== 'undefined' ? positives : undefined,
-    negatives: typeof negatives !== 'undefined' ? negatives : undefined,
-    zeroes: typeof zeroes !== 'undefined' ? zeroes : undefined,
+    negatives: typeof negatives !== 'undefined'? negatives : undefined,
+    zeroes: typeof zeroes !== 'undefined'? zeroes : undefined,
     stretchPositives: typeof stretchPositives !== 'undefined' ? stretchPositives : undefined,
     stretchNegatives: typeof stretchNegatives !== 'undefined' ? stretchNegatives : undefined,
     stretchZeroes: typeof stretchZeroes !== 'undefined' ? stretchZeroes : undefined,
@@ -101,3 +123,17 @@ try {
 } catch (e) {
   // Do nothing
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
